b0721a9a8cecf31cb3f525e3b1f8392c923c6bba
gr8ayu <anonymouskmr@gmail.com>
2020-04-07 12:22:55 +0530

    added swagger docs

--- backend/mysite/mysite/settings.py
+++ backend/mysite/mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
+    'drf_yasg',   
 
 ]
 
@@ -98,6 +99,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSIONS_CLASSES':(
             'rest_framework.permissions.IsAuthenticated',
         ),
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 
 AUTH_USER_MODEL = 'auths.User'
